10fa49ae7c29cb735fdef5e613544aef16113175
ZenXArch <zenxarch@noreply.codeberg.org>
2025-11-09 14:15:26 +0530

    fix compile

--- src/main/java/io/github/steveplays28/noisium/util/ModUtil.java
+++ src/main/java/io/github/steveplays28/noisium/util/ModUtil.java
@@ -1,12 +1,9 @@
 package io.github.steveplays28.noisium.util;
 
-import dev.architectury.injectables.annotations.ExpectPlatform;
+import net.fabricmc.loader.api.FabricLoader;
 
-@SuppressWarnings("unused")
-public abstract class ModUtil {
-  /** Checks if a mod is present during loading. */
-  @ExpectPlatform
+public class ModUtil {
   public static boolean isModPresent(String id) {
-    throw new AssertionError("Platform implementation expected.");
+    return FabricLoader.getInstance().isModLoaded(id);
   }
 }
